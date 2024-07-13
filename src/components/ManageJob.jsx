c7f86ffe26e93da2ce2a9bae861476fc69dd3b00
Aryan <69583042+aryannxroot@users.noreply.github.com>
2024-07-13 12:17:00 +0530

    changes

--- src/components/ManageJob.jsx
+++ src/components/ManageJob.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
+import JobContext from '../context/JobContext'
+
 const ManageJob = () => {
+  const job = useContext(JobContext);
+
   return (
     <div className="bg-white p-6 rounded-lg shadow-md mt-6">
         <h3 className="text-xl font-bold mb-6">Manage Job</h3>
         <Link
-        to="/add-job"
+        to={`/jobs/edit/${job.id}`}
         className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
         >Edit Job</Link>
         <button
